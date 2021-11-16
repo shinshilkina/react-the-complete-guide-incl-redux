55ba657a06373c705744c3a2ae870b3e0d843b73
shinshilkina <anastasiashilina98@gmail.com>
2021-11-16 16:41:04 +0700

    added expenssive component and change styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem/index'
+import Expenses from './components/Expenses/index';
 
 function App() {
   const expenses = [
@@ -22,24 +22,9 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ]
-  const expensesElements = expenses.map(element => {
-    if (!element.title || !element.amount || !element.date || !element.id) {
-      return;
-    }
-    return (
-      <div>
-      <ExpenseItem 
-        key={element.id}
-        title={element.title}
-        amount={element.amount}
-        date={element.date}
-      />
-      </div>
-    )
-  })
   return (
     <div>
-      { expensesElements }
+       <Expenses elements={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses/index.js
@@ -0,0 +1,21 @@
+import ExpenseItem from '../ExpenseItem/index';
+import './index.css';
+
+function Expenses(props) {
+  const expensesElements = props.elements.map(element => {
+    if (!element.title || !element.amount || !element.date || !element.id) {
+      return;
+    }
+    return (
+      <ExpenseItem 
+        key={element.id}
+        title={element.title}
+        amount={element.amount}
+        date={element.date}
+      />
+    )
+  })
+  return <div class="expenses"> { expensesElements } </div>;
+}
+
+export default Expenses;
