ed6860803659680f445e892029035839d5fd36b6
shinshilkina <anastasiashilina98@gmail.com>
2021-11-17 15:58:02 +0700

    64. Rendering Lists of Data

--- src/components/Expenses/Expense/index.js
+++ src/components/Expenses/Expense/index.js
@@ -5,20 +5,6 @@ import './index.css';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
-
-  const expensesElements = props.elements.map(element => {
-    if (!element.title || !element.amount || !element.date || !element.id) {
-      return;
-    }
-    return (
-      <ExpenseItem 
-        key={element.id}
-        title={element.title}
-        amount={element.amount}
-        date={element.date}
-      />
-    )
-  })
   
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
@@ -29,7 +15,14 @@ function Expenses(props) {
       selected={filteredYear}
       onChangeFilter={filterChangeHandler}
     />
-    { expensesElements } 
+    { props.elements.map(element => (
+      <ExpenseItem 
+        key={element.id}
+        title={element.title}
+        amount={element.amount}
+        date={element.date}
+      />
+    )) } 
   </div>;
 }
 
