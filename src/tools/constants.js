edf93b5b1c5a3d94b000426e6c73c75e98088b10
shinshilkina <anastasiashilina98@gmail.com>
2021-11-17 16:12:09 +0700

    65. Using Stateful

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,4 +1,26 @@
 const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 
   'July', 'Aug', 'Sept', 'Oct', 'Oct', 'Dec'];
 
-export { MONTHS };
+const DEFAULT_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+export { MONTHS, DEFAULT_EXPENSES };
