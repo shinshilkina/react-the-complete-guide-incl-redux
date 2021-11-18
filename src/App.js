c7cfec2f911ac7792e7bdc98a8df79e5373950cc
shinshilkina <anastasiashilina98@gmail.com>
2021-11-18 14:44:39 +0700

    get starting files from the udemy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React, {useState} from 'react';
-import Expenses from './components/Expenses/Expense/index';
-import NewExpense from './components/NewExprense/NewExpense';
-import { DEFAULT_EXPENSES } from './tools/constants';
+import React from 'react';
+
 
 function App() {
-  const [expenses, setExpense] = useState(DEFAULT_EXPENSES);
-
-  const addExprenseHandler = expense => {
-    setExpense(prevExprenses => [expense, ...prevExprenses]);
-  };
-
-  // return React.createElement(
-  //   'div', 
-  //   {}, 
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, {elements: expenses})
-  // );
   return (
     <div>
-      <NewExpense onAddExpense={ addExprenseHandler }/>
-      <Expenses elements={expenses}/>
+
     </div>
   );
 }
