b75dc2957c777e9fb8d02371a99809e3d0ab90b3
shinshilkina <anastasiashilina98@gmail.com>
2021-11-22 14:03:45 +0700

    init branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, {useState} from 'react';
-import Expenses from './components/Expenses/Expense/index';
-import NewExpense from './components/NewExprense/NewExpense';
-import { DEFAULT_EXPENSES } from './tools/constants';
+import React from 'react';
+
+import './App.css';
 
 function App() {
-  const [expenses, setExpense] = useState(DEFAULT_EXPENSES);
-
-  const addExprenseHandler = expense => {
-    setExpense(prevExprenses => [expense, ...prevExprenses]);
-  };
-
-  // return React.createElement(
-  //   'div', 
-  //   {}, 
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, {elements: expenses})
-  // );
   return (
-    <div>
-      <NewExpense onAddExpense={ addExprenseHandler }/>
-      <Expenses elements={expenses}/>
+    <div className="app">
+      <h1>Hi there!</h1>
     </div>
   );
 }
