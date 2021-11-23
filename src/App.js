931a8416962257478ec8074037fa81f779e29b78
shinshilkina <anastasiashilina98@gmail.com>
2021-11-23 15:47:35 +0700

    install starter package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import React, {useState} from 'react';
-import Expenses from './components/Expenses/Expense/index';
-import NewExpense from './components/NewExprense/NewExpense';
-import { DEFAULT_EXPENSES } from './tools/constants';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [expenses, setExpense] = useState(DEFAULT_EXPENSES);
-
-  const addExprenseHandler = expense => {
-    setExpense(prevExprenses => [expense, ...prevExprenses]);
-  };
-
-  // return React.createElement(
-  //   'div', 
-  //   {}, 
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, {elements: expenses})
-  // );
   return (
-    <div>
-      <NewExpense onAddExpense={ addExprenseHandler }/>
-      <Expenses elements={expenses}/>
+    <div className="app">
+      <SimpleInput />
     </div>
   );
 }
