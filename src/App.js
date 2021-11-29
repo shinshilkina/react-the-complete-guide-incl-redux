4fa9686f036131d894ec9c43eaff68841f875615
shinshilkina <anastasiashilina98@gmail.com>
2021-11-29 17:33:38 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, {useState} from 'react';
-import Expenses from './components/Expenses/Expense/index';
-import NewExpense from './components/NewExprense/NewExpense';
-import { DEFAULT_EXPENSES } from './tools/constants';
+import Counter from './components/Counter';
+
 
 function App() {
-  const [expenses, setExpense] = useState(DEFAULT_EXPENSES);
-
-  const addExprenseHandler = expense => {
-    setExpense(prevExprenses => [expense, ...prevExprenses]);
-  };
-
-  // return React.createElement(
-  //   'div', 
-  //   {}, 
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, {elements: expenses})
-  // );
   return (
-    <div>
-      <NewExpense onAddExpense={ addExprenseHandler }/>
-      <Expenses elements={expenses}/>
-    </div>
+    <Counter />
   );
 }
 
